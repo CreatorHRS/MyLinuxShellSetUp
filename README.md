95b57faa8e87839784a92953e3eefe027f1074ca
mikhailkhr <CreatorDT1@gmail.com>
2022-05-02 19:39:02 +0300

    fead: add script and setting file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MyLinuxShellSetUp
 Script that set up all setting for my preferred shell
+
+Clone git repo
+`git clone https://github.com/CreatorHRS/MyLinuxShellSetUp.git && cd ./MyLinuxShellSetUp`
+
+Run script
+`./set_up.sh`
---
+++ set_up.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#installing packeges
+sudo pacman -S zsh powerline powerline-fonts zsh-syntax-highlighting git curl
+
+#installing oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#copy settings
+cat ./.zshrc > ~/.zshrc
+
+#create a alias file
+touch ~/.zsh_aliases
+
+#open zsh
+zsh
